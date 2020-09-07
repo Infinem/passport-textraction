a3c1574470a88526f81ab6cb1365ad33406c6f71
Sunnat Makhmudov <sunnat_m98@icloud.com>
2020-09-07 16:44:32 +0500

    modified the link to heroku app

--- examples/client.py
+++ examples/client.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_response(path):
-   url = "https://passport-textraction.herokuapp.com/detect"
+   url = "https://github-textraction.herokuapp.com/detect"
    files = {'image': open(path, 'rb')}
    response = requests.post(url, files=files)
    print(response.text)
